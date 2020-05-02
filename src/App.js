5ace9b033f7ec43bad4deaec511d746d58586e3c
vhajizadeh <v.hajizadeh66@gmail.com>
2020-05-02 22:58:33 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BookList from "./BookList";
+
+
+const App=()=>(
+    <div>
+        <h2 className='head'>information list</h2>
+
+        <BookList/>
+    </div>
+)
+
+
+export default App;
